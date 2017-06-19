7b2e04d9f8e93a92d9bda6d4ad8aae6d7895d9a3
Keuthonymous <mikie.daughtrey@gmail.com>
2017-06-19 10:29:07 +0200

    Added custom js file to Scripts and addded a new bundle to the bundle config

---
+++ MVCGarageAngularJS/Scripts/MVCgarage.js
@@ -0,0 +1,21 @@
+﻿$(function () {
+
+    var ajaxFormSubmit = function () {
+        var $form = $(this);
+
+        var options = {
+            url: $form.attr("action"),
+            type: $form.attr("method"),
+            data: $form.serialize()
+        };
+
+        $.ajax(options).done(function (data) {
+            var $target = $($form.attr("data-MVCgarage-target"));
+            $target.replaceWith(data);
+        });
+
+        return false;
+
+    };
+
+})();
