f8041b40e9cf980647ccaf4da7f95ff6af6ce752
Keuthonymous <mikie.daughtrey@gmail.com>
2017-06-19 15:53:47 +0200

    Added OwnerAPIController and started working with some of the more ocmplicated things with

---
+++ MVCGarageAngularJS/Controllers/OwnersAPIController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using MVCGarageAngularJS.DataAccess;
+using MVCGarageAngularJS.Models;
+using MVCGarageAngularJS.Repositories;
+
+namespace MVCGarageAngularJS.Controllers
+{
+    public class OwnersAPIController : ApiController
+    {
+        private OwnersRepository db = new OwnersRepository();
+
+        // GET: api/OwnersAPI
+        public IEnumerable<Owner> Get()
+        {
+            return db.GetAllOwners();
+        }
+
+        // GET: api/OwnersAPI/5
+        [ResponseType(typeof(Owner))]
+        public IHttpActionResult Get(int id)
+        {
+            Owner owner = db.Find(id);
+            if (owner == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(owner);
+        }
+
+        // PUT: api/OwnersAPI/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutOwner(int id, Owner owner)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != owner.ID)
+            {
+                return BadRequest();
+            }
+
+            db.EditOwner(owner);
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/OwnersAPI
+        [ResponseType(typeof(Owner))]
+        public IHttpActionResult PostOwner(Owner owner)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.AddNewOwner(owner);
+            
+
+            return CreatedAtRoute("DefaultApi", new { id = owner.ID }, owner);
+        }
+
+        // DELETE: api/OwnersAPI/5
+        [ResponseType(typeof(Owner))]
+        public IHttpActionResult DeleteOwner(int id)
+        {
+            Owner owner = db.Find(id);
+            if (owner == null)
+            {
+                return NotFound();
+            }
+
+            db.RemoveOwner(owner);
+            
+            return Ok(owner);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool OwnerExists(int id)
+        {
+            IEnumerable<Owner> Owners = db.GetAllOwners();
+            return Owners.Count(e => e.ID == id) > 0;
+        }
+    }
+}
--- MVCGarageAngularJS/Scripts/vehicles.js
+++ MVCGarageAngularJS/Scripts/vehicles.js
@@ -6,9 +6,13 @@
 
         $scope.getVehicle = getVehicle;
         $scope.getVehicles = getVehicles;
+        $scope.getOwner = getOwner;
+        $scope.getOwners = getOwners;
         $scope.sendData = sendData;
 
-        $scope.vehicle = { ID: 0,  RegistrationPlate: ""};
+
+        $scope.vehicle = { ID: 0, RegistrationPlate: "" };
+        $scope.owner = { ID: 0, Name: "" };
 
         function getVehicles() {
             $http.get("/api/vehiclesAPI/get")
@@ -21,14 +25,29 @@
         function getVehicle(id) {
             $http.get("/api/vehiclesAPI/get?id=" + id)
             .then(function (response) {
-                $scope.vehicleType = response.data;
+                $scope.vehicle = response.data;
+            });
+        }
+
+        function getOwners() {
+            $http.get("api/ownersAPI/get")
+                .then(function (response) {
+                    $scope.ownerData = response.data;
+                });
+        }
+
+        function getOwner(id) {
+            $http.get("api/ownersAPI/get?id=" + id)
+            .then(function (response) {
+                $scope.owner = response.data;
+                debugger;
             });
         }
 
         function sendData() {
-            $http.post("/api/vehiclesAPI/post", JSON.stringify($scope.vehicleType))
+            $http.post("/api/vehiclesAPI/post", JSON.stringify($scope.vehicle))
             .then(function (response) {
-                var tmp = angular.copy($scope.vehicleType);
+                var tmp = angular.copy($scope.vehicle);
                 $scope.data.push(tmp);
                 $scope.vehicle.Owner = undefined;
                 $scope.vehicle.VehicleType = undefined;
