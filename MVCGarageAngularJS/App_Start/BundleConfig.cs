637c079061a0847c3ed641f8ff1658af2a1fbd44
Keuthonymous <mikie.daughtrey@gmail.com>
2017-06-19 10:37:50 +0200

    Added angular script and added it to the bundles

--- MVCGarageAngularJS/App_Start/BundleConfig.cs
+++ MVCGarageAngularJS/App_Start/BundleConfig.cs
@@ -11,7 +11,8 @@ namespace MVCGarageAngularJS
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/CustJS").Include(
+            bundles.Add(new ScriptBundle("~/bundles/Angular").Include(
+                        "~/Scripts/angular.min.js",        
                         "~/Scripts/MVCgarage.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
